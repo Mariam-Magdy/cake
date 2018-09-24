7dd0dc9c49f259b674a5aa8efe0c2a345cb26bf3
mariam <magdymariam3@gmail.com>
2018-09-24 12:24:53 +0200

    version 0.7

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var next = document.getElementById('next');
+var prev = document.getElementById('prev');
+var slideshow = document.getElementById('slideshow');
+
+function x() {
+    slideshow.style.backgroundImage="url('assets/show2.jpg')";
+}
+
+function y() {
+    slideshow.style.backgroundImage="url('assets/show3.jpeg')";
+}
